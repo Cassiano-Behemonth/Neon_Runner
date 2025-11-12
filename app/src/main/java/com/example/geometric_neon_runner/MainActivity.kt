6dc75d6a4378893b7df1da0d1c87746c387c1560
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-11-12 06:56:23 -0300

    Commit Inicial

---
+++ app/src/main/java/com/example/geometric_neon_runner/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.geometric_neon_runner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.geometric_neon_runner.ui.theme.Geometric_neon_runnerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Geometric_neon_runnerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Geometric_neon_runnerTheme {
+        Greeting("Android")
+    }
+}
