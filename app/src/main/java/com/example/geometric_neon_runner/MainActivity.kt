ce30047b3c53d24074c37e97063d14fe30081bd3
Gust-Henrique <gustavohds2019@outlook.com>
2025-11-13 15:52:36 -0300

    Activity Principal

--- app/src/main/java/com/example/geometric_neon_runner/MainActivity.kt
+++ app/src/main/java/com/example/geometric_neon_runner/MainActivity.kt
@@ -1,47 +1,32 @@
 package com.example.geometric_neon_runner
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.geometric_neon_runner.ui.theme.Geometric_neon_runnerTheme
+import com.example.geometric_neon_runner.ui.navigation.AppNavigation
+import com.example.geometric_neon_runner.ui.theme.NeonTunnelTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            Geometric_neon_runnerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            NeonTunnelTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Geometric_neon_runnerTheme {
-        Greeting("Android")
     }
 }
